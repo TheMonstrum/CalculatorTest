a1e9309a31bfb8983a69dfe212422131a07c40b2
Varga Roland <roland.varga@attrecto.com>
2021-01-26 20:52:21 +0100

    init

---
+++ src/Calculator.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Attrectotest\Calculator;
+
+
+class Calculator
+{
+
+    /**
+     * Adds together two numbers
+     *
+     * @param integer $a first number
+     * @param integer $b second number
+     *
+     * @return integer
+     */
+    public function AddNumbersTogether($a, $b){
+        return $a + $b;
+    }
+}
